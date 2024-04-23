1495529295b90082430821229e9fc48f76cf9762
js-yu1592 <wnstkd31@gmail.com>
2024-04-23 16:37:32 +0900

    Initial Commit

---
+++ LMessenger/LMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//  Created by 유준상 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
